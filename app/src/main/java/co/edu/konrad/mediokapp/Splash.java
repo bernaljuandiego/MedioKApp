dbb466ce0c3079ed2eeeba63c8eea6300554382e
bernaljuandiego <bernaljuandiego@hotmail.com>
2018-08-30 23:12:17 -0500

    add splash and login layout
    the images of the project, the logo, and the login and splash layouts were added

--- app/src/main/java/co/edu/konrad/mediokapp/Splash.java
+++ app/src/main/java/co/edu/konrad/mediokapp/Splash.java
@@ -1,5 +1,7 @@
 package co.edu.konrad.mediokapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                cambioPantalla();
+            }
+        }, 2000);
+    }
+
+    private void cambioPantalla() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+
     }
 }
