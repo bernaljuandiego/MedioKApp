9a5052506152197a25112c66b45b160fb8ea9058
Juan DIego Bernal <bernaljuandiego@hotmail.com>
2018-11-15 21:20:46 -0500

    edited fragments and home page

--- app/src/main/java/co/edu/konrad/mediokapp/activities/ExercisesFragment.java
+++ app/src/main/java/co/edu/konrad/mediokapp/activities/ExercisesFragment.java
@@ -1,42 +1,54 @@
-package co.edu.konrad.mediokapp.activities;
+package co.edu.konrad.mediokapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-import co.edu.konrad.mediokapp.R;
 import co.edu.konrad.mediokapp.adapter.GymAdapter;
 import co.edu.konrad.mediokapp.entities.GymExercises;
 
-public class GymExerciseActivity extends AppCompatActivity {
+
+public class ExercisesFragment extends Fragment {
 
     private ListView items;
     private GymAdapter adaptador;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gym_exercises);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_exercises, container, false);
+    }
 
-        items = (ListView) findViewById(R.id.lvItems);
-        adaptador = new GymAdapter(this, GetArrayItems());
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Ejercicios Musculares");
+        items = (ListView) getView().findViewById(R.id.lvItems);
+        adaptador = new GymAdapter(getContext(), GetArrayItems());
+
+        items.setAdapter(adaptador);items = (ListView) getView().findViewById(R.id.lvItems);
+        adaptador = new GymAdapter(getContext(), GetArrayItems());
 
         items.setAdapter(adaptador);
     }
 
     private ArrayList<GymExercises> GetArrayItems() {
         ArrayList<GymExercises> listItems = new ArrayList<>();
-
         listItems.add(new GymExercises(R.mipmap.ic_abdominales,"Abdominales",getResources().getString(R.string.descAbdominales)));
         listItems.add(new GymExercises(R.mipmap.ic_brazos,"Brazos",getResources().getString(R.string.descBrazos)));
         listItems.add(new GymExercises(R.mipmap.ic_espalda,"Espalda",getResources().getString(R.string.descEspalda)));
         listItems.add(new GymExercises(R.mipmap.ic_hombros,"Hombros",getResources().getString(R.string.descHombros)));
         listItems.add(new GymExercises(R.mipmap.ic_pecho,"Pecho",getResources().getString(R.string.descPecho)));
         listItems.add(new GymExercises(R.mipmap.ic_piernas,"Piernas",getResources().getString(R.string.descPiernas)));
-
-
         return listItems;
     }
 }
--- app/src/main/java/co/edu/konrad/mediokapp/activities/MusicFragment.java
+++ app/src/main/java/co/edu/konrad/mediokapp/activities/MusicFragment.java
@@ -1,42 +1,52 @@
-package co.edu.konrad.mediokapp.activities;
+package co.edu.konrad.mediokapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-import co.edu.konrad.mediokapp.R;
+import co.edu.konrad.mediokapp.adapter.GymAdapter;
 import co.edu.konrad.mediokapp.adapter.MusicAdapter;
+import co.edu.konrad.mediokapp.entities.GymExercises;
 import co.edu.konrad.mediokapp.entities.MusicGender;
 
-public class MusicGenderActivity extends AppCompatActivity {
+
+public class MusicFragment extends Fragment {
 
     private ListView items;
     private MusicAdapter adaptador;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_music, container, false);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_music_gender);
-
-        items = (ListView) findViewById(R.id.lvItems);
-        adaptador = new MusicAdapter(this, GetArrayItems());
-
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Musica Bailable");
+        items = (ListView) getView().findViewById(R.id.lvItems);
+        adaptador = new MusicAdapter(getContext(), GetArrayItems());
         items.setAdapter(adaptador);
     }
 
     private ArrayList<MusicGender> GetArrayItems() {
         ArrayList<MusicGender> listItems = new ArrayList<>();
-
         listItems.add(new MusicGender(R.drawable.salsa_dancers, getResources().getString(R.string.descSalsa), 89));
         listItems.add(new MusicGender(R.drawable.reggeaton_dancers, getResources().getString(R.string.descReggeaton), 125));
         listItems.add(new MusicGender(R.drawable.pop_dancers, getResources().getString(R.string.descPop), 210));
         listItems.add(new MusicGender(R.drawable.electronica_dancers, getResources().getString(R.string.descElectronica), 187));
         listItems.add(new MusicGender(R.drawable.bachata_dancers, getResources().getString(R.string.descBachata), 95));
         listItems.add(new MusicGender(R.drawable.ballenato_dancers, getResources().getString(R.string.descBallenato), 150));
-
         return listItems;
     }
 }
